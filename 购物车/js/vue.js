763ef3b222a6578b0b2e52b3ff16760ca4926ed7
小TIAN.★☆ <1756768668@qq.com>
2017-06-06 16:38:40 +0800

    购物车

--- 购物车/js/vue.js
+++ 购物车/js/vue.js
@@ -1,1 +1,22 @@
 // JavaScript source code
+var data = [
+
+    {
+        name: 'IPhone 6',
+        price: 5466,
+        id: 1,
+        count:1
+    },
+    {
+        name: 'IMac',
+        price: '7466',
+        id: 2,
+        count:1
+    },
+    {
+        name: 'iPad',
+        price: 5400,
+        id: 3,
+        count:1
+    }
+]
